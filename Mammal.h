45e447659a4a6ea72b36886680922f568c820568
habibasalah-16001573 <habibasalah21@gmail.com>
2025-11-12 15:38:55 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,12 +2,20 @@
 #define MAMMAL_H
 
 #include "Animal.h"
+    class Mammal : public Animal{
+private:
+    string furColor;
 
-//define mammal class here
+public:
+    Mammal(string name,int age,bool hungry,string furColor)
+        : Animal(age,name,isHungry ) {
+        Fur color= furColor;
+    }
 
-
-
-
-
+    void displayInfo() {
+        Animal::displayInfo();
+        cout << "Fur color: " << furColor << endl;
+    }
+    };
 
 #endif
